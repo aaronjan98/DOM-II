3d58e8b678e33a41247d37d3186d2d68637f1827
aaronjan <aaronjan98@gmail.com>
2019-12-03 14:08:52 -0800

    need to come back to the ondragend event

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
+// created a link tag in <head>
+const font = document.createElement('link');
+font.href = 'https://fonts.googleapis.com/css?family=Lobster&display=swap';
+font.rel = 'stylesheet';
+
 // FunBus Logo Hover Effect using Event Listeners: mouseenter & mouseleave
 const FunBus = document.querySelector('.logo-heading');
-console.log(FunBus);
 
 FunBus.addEventListener('mouseenter', () => {
     FunBus.style.transform = "scale(2)";
@@ -10,16 +14,25 @@ FunBus.addEventListener("mouseleave", () => {
     FunBus.style.transform = "scale(1)";
 })
 
-// click event for nav items
+// click & dblclick events for nav items
 const nav = document.querySelectorAll('.nav-link').forEach(el => {
     el.addEventListener('click', () => {
         el.style.color = 'red';
         el.style.fontFamily = 'Lobster, cursive';
-        el.style.fontSize = '2.6rem';
+        el.style.fontSize = '4rem';
+        document.querySelector('.nav').style.width = '80%';
+    })
+    el.addEventListener('dblclick', () => {
+        document.querySelector('header').style.backgroundColor = 'aqua';
+        el.style.color = 'black';
+        el.style.fontFamily = 'Robo';
+        el.style.fontSize = '2rem';
     })
 })
 
-// created a link tag in <head>
-const font = document.createElement('link');
-font.href = 'https://fonts.googleapis.com/css?family=Lobster&display=swap';
-font.rel = 'stylesheet';
+// drag event for img/fun-bus.jpg
+const dragg = document.querySelector('.intro img');
+
+dragg.addEventListener('ondragend', () => {
+    window.alert('hi');
+})
