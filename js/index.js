a12fa385ef1a0a166f93c4a326d511f089766b78
aaronjan <aaronjan98@gmail.com>
2019-12-03 12:28:23 -0800

    added two even listeners to .logo-heading

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+const FunBus = document.querySelector('.logo-heading');
+console.log(FunBus);
+
+FunBus.addEventListener('mouseenter', () => {
+    FunBus.style.transform = "scale(2)";
+    FunBus.style.transition = "transform 0.3s"
+})
+FunBus.addEventListener("mouseleave", () => {
+    FunBus.style.transform = "scale(1)"
+})
