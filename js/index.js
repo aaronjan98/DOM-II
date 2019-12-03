1c77d6aa8f09414f4dedb21a6483682b83f8474e
aaronjan <aaronjan98@gmail.com>
2019-12-03 12:35:41 -0800

    added click event to .nav-links

--- js/index.js
+++ js/index.js
@@ -1,11 +1,18 @@
-// Your code goes here
+// FunBus Logo Hover Effect using Event Listeners: mouseenter & mouseleave
 const FunBus = document.querySelector('.logo-heading');
 console.log(FunBus);
 
 FunBus.addEventListener('mouseenter', () => {
     FunBus.style.transform = "scale(2)";
-    FunBus.style.transition = "transform 0.3s"
+    FunBus.style.transition = "transform 0.3s";
 })
 FunBus.addEventListener("mouseleave", () => {
-    FunBus.style.transform = "scale(1)"
+    FunBus.style.transform = "scale(1)";
+})
+
+// click event for nav items
+const nav = document.querySelectorAll('.nav-link').forEach(el => {
+    el.addEventListener('click', () => {
+        el.style.color = 'red';
+    })
 })
